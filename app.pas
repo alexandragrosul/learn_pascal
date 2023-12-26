923c80b6893d0324bd9a932399a3b4402ba76672
Grosul Alexandra <grosulalexandra@gmail.com>
2023-12-26 16:50:06 +0200

    first lesson

---
+++ app.pas
@@ -0,0 +1,10 @@
+Program P1;
+{Suma numerelor intregi x,y,z}
+var x, y, z, s : Integer;
+begin 
+writeln('Introduceti numerele intregi x,y,z');
+readln(x,y,z);
+s:=x + y + z;
+writeln('Suma numerele intregi x,y,z');
+writeln(s)
+end.
